9b52e045b36ad956a4f3ebbcdf911e60c1efe330
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-11-08 19:42:37 -0500

    ADD: git add .

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,4 @@
-import * as express from 'express';
-import * as http from 'http';
-import * as  socket from 'socket.io';
+import { Server } from "./app/infraestructure/server";
 
-
-const app = express.default();
-app.use(express.static(__dirname + '/../public'));
-const server = http.createServer(app);
-const io = new socket.Server(server);
-
-io.on('connection', (socket) => {
-    console.log("CLiente conectado");
-    console.log(socket.id);
-    socket.emit('message', { message: 'Bienvenido a la aplicacion', fecha: new Date() });
-    socket.on('message-to-server', (data) => {
-        console.log(data);
-        io.emit("message-from-server", data);
-    });
-
-});
-server.listen(3000, () => {
-    console.log("Server corriendo en puerto :8080");
-
-});
+const server = new Server();
+server.execute();
